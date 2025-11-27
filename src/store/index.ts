a83e1c5a273e18d1671bf5ad73c8e1106681363a
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2025-11-27 09:17:46 -0300

    feat: create prop user of the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,13 +2,21 @@ import { create } from 'zustand';
 
 type Store = {
   counter: number;
+  user: {
+    username: string;
+  }
 }
 
 type Actions = {
   increment: () => void;
+  setUsername: (username: string) => void;
 }
 
-export const useStore = create<Store & Actions>((set) => ({
+export const useStore = create<Store & Actions>()((set) => ({
   counter: 1,
+  user: {
+    username: 'gabriel-jesusvix'
+  },
   increment: () => set((prevState) => ({ counter: prevState.counter + 1 })),
+  setUsername: (username: string) => set({ user: { username }}),
 }));
