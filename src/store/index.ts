08454009c6315e7196f930f7ca73b62316ab2df0
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2025-11-28 07:13:49 -0300

    feat: use immer to change object

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,12 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
 
 type Store = {
   counter: number;
   user: {
+    name: string;
+    email: string;
     username: string;
   }
 }
@@ -12,11 +16,25 @@ type Actions = {
   setUsername: (username: string) => void;
 }
 
-export const useStore = create<Store & Actions>()((set) => ({
-  counter: 1,
-  user: {
-    username: 'gabriel-jesusvix'
-  },
-  increment: () => set((prevState) => ({ counter: prevState.counter + 1 })),
-  setUsername: (username: string) => set({ user: { username }}),
-}));
+
+export const useStore = create<Store & Actions>()(
+  devtools(
+    immer(
+      (set) => ({
+        counter: 1,
+        user: {
+          name: 'Gabriel Jesus',
+          email: 'gabriel.dev@mail.com',
+          username: 'gabriel-jesusvix'
+        },
+        increment: () => set((prevState) => ({ counter: prevState.counter + 1 })),
+        setUsername: (username: string) => set(prevState => {
+          prevState.user.username = username;
+        }),
+      })
+    ),
+    {
+      enabled: import.meta.env.DEV,
+    }
+  ),
+);
